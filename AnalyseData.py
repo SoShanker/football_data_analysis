65447a8ff9423385bae17d5c0ee2953b816e37c5
SoShanker <67009169+SoShanker@users.noreply.github.com>
2025-02-10 18:31:11 +0100

    function to compute ranking

--- AnalyseData.py
+++ AnalyseData.py
@@ -12,16 +12,6 @@ class AnalyzeFootballData:
             "Liga":"spanish-la-liga"
         }
         self.ongoingseason = "2024-2025"
-
-    def compute_actual_teams(self,league:str):
-        '''
-        Check ongoing season teams
-        Input : League name
-        Output : List of teams name
-        '''
-        ongoing_season_df = pd.read_csv(os.path.join(self.path,league,f"season{self.ongoingseason}.csv"))
-        teams = list(ongoing_season_df["HomeTeam"].drop_duplicates())
-        return teams
     
     def compute_ranking(self,league:str,year:int):
         '''
@@ -29,7 +19,28 @@ class AnalyzeFootballData:
         Input : League name and starting year of the season
         Output : DataFrame of the results
         '''
-        #TODO
+        season_df = pd.read_csv(os.path.join(self.path,league,f"season{year}-{year+1}.csv"))
+        teams = list(season_df["HomeTeam"].drop_duplicates())
+        scores = {team : 0 for team in teams}
+
+        for i in range (len(season_df)):
+            match = season_df.iloc[i]
+            home_team,away_team = match["HomeTeam"],match["AwayTeam"]
+            result = match["FTR"]
+            if result == "H":
+                scores[home_team]+=3
+            elif result == "A":
+                scores[away_team]+=3
+            else:
+                scores[away_team]+=1
+                scores[home_team]+=1
+            
+        scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1],reverse=True)}
+
+        final_ranking = pd.DataFrame()
+        final_ranking["Team"] = list(scores.keys())
+        final_ranking["Score"] = list(scores.values())
+        return final_ranking
 
     def compute_win_lose_history(self,league:str):
         '''
@@ -58,5 +69,3 @@ class AnalyzeFootballData:
 
 if __name__ == "__main__":
     analysis = AnalyzeFootballData("FootballData")
-    for league in analysis.leagues:
-        print(analysis.compute_actual_teams(league))
