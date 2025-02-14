e011c77fefa9c95ed7afbf42d06eeb2a3ea61c11
SoShanker <67009169+SoShanker@users.noreply.github.com>
2025-02-14 17:27:15 +0100

    maj des penalités Ligue1

--- Variables.py
+++ Variables.py
@@ -1,7 +1,37 @@
 POINTS_SANCTION={
     "Ligue1":{
-        "2021-2022":{
-            "Nice":-1
+        2006:{
+            "Nantes":-1, #Envahissement de terrain lors du match nul contre Toulouse (perte du point du match nul),
+            "Toulouse":2 #Gain du match sur tapis vert contre Nantes (+3 à la place de +1 du match nul)
+        },
+        2012:{
+            "Ajaccio":-2 #en raison d'incidents lors du match de L1 face à Lyon le 13 mai.
+        },
+        2013:{
+            "Nantes":-3, #pour avoir aligné un joueur qui était suspendu lors de la victoire contre Bastia (2-0) en début de saison.
+            "Bastia":3 #Bastia prend donc la victoire
+        },
+        2019:{ #Arrêt de la saison en cours (cause Covid) Reims finit donc derrière Nice a la confrontation direct et Strasbourg devant Angers au ratio Points / Matchs joués
+            #TODO AFFICHER LES SANCTIONS
+        },
+        2021:{
+            "Nice":-1, #Cause supporters, perte d'un point
+            "Lyon":-1 #Cause supporters, perte d'un point OM-OL
+        },
+        2023:{
+            "Montpellier":-1
         }
+    },
+    "PremierLeague":{
+
+    },
+    "SerieA":{
+
+    },
+    "Bundesliga":{
+
+    },
+    "Liga":{
+
     }
 }
